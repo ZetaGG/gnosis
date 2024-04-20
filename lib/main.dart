b15b451b45e7b39288117456736389f94f08995b
Zeta <zetag174@gmail.com>
2024-04-19 19:47:48 -0600

    Begin Login page
    Login Page y Register Page en proceso

--- lib/main.dart
+++ lib/main.dart
@@ -1,18 +1,62 @@
 import 'package:flutter/material.dart';
+import 'package:gnosis/login.dart';
+import 'package:gnosis/register.dart';
+import 'package:gradients/gradients.dart';
+
 
 void main() {
-  runApp(const MainApp());
+  runApp(MyApp());
 }
 
-class MainApp extends StatelessWidget {
-  const MainApp({super.key});
-
+class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: Scaffold(
-        body: Center(
-          child: Text('Hello World!'),
+    return MaterialApp(
+      title: 'Inicio',
+      home: MyHomePage(),
+      routes: {
+        '/login': (context) => LoginPage(),
+        '/home': (context) => MyHomePage(),
+      },
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Inicio de sesión/Registro'),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Image.asset('assets/logo.png'),
+            SizedBox(height: 20),
+            Text('Bienvenido'),
+            SizedBox(height: 20),
+            ElevatedButton(
+              onPressed: () {
+                Navigator.push(context, MaterialPageRoute(builder: (context)=> LoginPage()));
+              },
+              child: Text('Iniciar sesión'),
+            ),
+            SizedBox(height: 10),
+            
+            ElevatedButton(
+              onPressed: () {
+                Navigator.push(context, MaterialPageRoute(builder: (context)=> RegisterPage()));
+                },
+              child: Text('Registrarse'),
+            ),
+          ],
         ),
       ),
     );
